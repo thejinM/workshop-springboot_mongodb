dab55b8c3ccb327c161d3daa78c47ea0b3c472e0
Jean Mallmann <jean.mallmann.pb@compasso.com.br>
2025-01-20 16:57:55 -0300

    Criação do projeto.

---
+++ src/main/java/com/Programa.java
@@ -0,0 +1,15 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Programa 
+{
+
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(Programa.class, args);
+	}
+
+}
