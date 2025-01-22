271e55a5f14d26415d7cc5f211ed167962aae477
Jean Mallmann <jean.mallmann.pb@compasso.com.br>
2025-01-22 15:03:55 -0300

    Endpoint para retornar os 'posts' de um 'Usuário'.

--- src/main/java/com/resources/RecursoUsuario.java
+++ src/main/java/com/resources/RecursoUsuario.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.domain.Post;
 import com.domain.Usuario;
 import com.dto.UsuarioDTO;
 import com.services.ServicoUsuario;
@@ -65,5 +66,12 @@ public class RecursoUsuario
     obj.setId(id);
     obj = servicoUsuario.atualizar(obj);
     return ResponseEntity.noContent().build();
+  }
+
+  @GetMapping(value = "/{id}/posts")
+  public ResponseEntity<List<Post>> findPosts(@PathVariable String id)
+  {
+    Usuario obj = servicoUsuario.findById(id);
+    return ResponseEntity.ok().body(obj.getPosts());
   }
 }
